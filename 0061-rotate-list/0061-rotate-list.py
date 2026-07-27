0fcf97748cf8a751eee474efa8f62765dd334c63
Desmode-yatharth <147180171+Desmode-yatharth@users.noreply.github.com>
2026-07-27 13:24:27 +0530

    Time: 20 ms (6.19%), Space: 12.4 MB (84.76%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -1,18 +1,40 @@
-class Solution:
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
     def rotateRight(self, head, k):
-        if not head or not head.next or k == 0: return head  
-        length = 1
-        tail = head
-        while tail.next:
-            tail = tail.next
-            length += 1  
-        k %= length
-        if k == 0: return head 
-        tail.next = head  # circular
-        steps = length - k
-        newtail = head
-        for _ in range(1, steps):
-            newtail = newtail.next  
-        newhead = newtail.next  
-        newtail.next = None  
-        return newhead
+        if not head or not head.next or k == 0 : return head
+
+        len_ll = 1
+        curr = head
+        while curr.next :
+            len_ll += 1
+            curr = curr.next
+        k = k % len_ll
+        if k == 0 : return head
+        
+        old_tail = curr
+        old_tail.next = head
+
+        n = len_ll - k
+        
+        curr = head
+        while n > 1 :
+            curr = curr.next
+            n -= 1
+        
+        new_tail = curr
+        new_head = new_tail.next
+        new_tail.next = None
+    
+        return new_head
+            
+
+        """
+        :type head: Optional[ListNode]
+        :type k: int
+        :rtype: Optional[ListNode]
+        """
+        
