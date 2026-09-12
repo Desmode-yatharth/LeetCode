e47e77d11d6b1c08a5c3476c1a8d76eda21767b2
Desmode-yatharth <147180171+Desmode-yatharth@users.noreply.github.com>
2026-09-12 15:33:16 +0530

    Time: 8 ms (82.03%), Space: 22.4 MB (84.68%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -3,11 +3,11 @@ class Solution:
         in_deg = [0] * (n+1)
         out_deg = [0] * (n+1)
         for u,v in trust :
-            in_deg[u] += 1
-            out_deg[v] += 1
+            in_deg[v] += 1
+            out_deg[u] += 1
         j = -1
         for itr in range(1,n + 1):
-            if in_deg[itr] == 0 and out_deg[itr] == n - 1 :
-                if j == -1: j = itr
-                else : return -1
+            if in_deg[itr] == n - 1 and out_deg[itr] == 0 :
+                j = itr
+            
         return j
